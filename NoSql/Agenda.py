e04b205062fb6ac7bf9c75e197ae11e504d86df2
juaangr <rodriguezgomezj195@gmail.com>
2025-01-21 18:50:29 +0100

    21/1

---
+++ NoSql/Agenda.py
@@ -0,0 +1,29 @@
+import dbm
+
+
+db = dbm.open('pracNoSql.db', 'c')
+
+
+
+def menu():
+    print("BIENVENIDO A LA AGENDA NO SQL")
+    print("1. Dar de alta un usuario")
+    print("2. Dar de baja un usuario")
+    print("3. Modificar un usuario")
+    print("4. Mostrar todos los usuarios")
+    print("5. Salir/Terminar el programa")
+
+
+
+    
+def alta():
+    pass
+
+def baja():
+    pass
+
+def modify():
+    pass
+
+def show():
+    pass
